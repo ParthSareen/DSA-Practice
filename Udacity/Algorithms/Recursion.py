cae3b4fac770f6ac3c25039e650443ef1f457ca9
ParthSareen <parth0612@live.com>
2019-02-16 15:40:55 -0500

    adding recursion

--- Udacity/Algorithms/Recursion.py
+++ Udacity/Algorithms/Recursion.py
@@ -1,3 +1,28 @@
 """
 Recursion
+function calling itself
+base case,
+calling itself
+base case is exit condition
+needs to alter input parameter
+
+
 """
+#non recursive fib in c++:
+function getFib(position) {
+  if (position == 0) { return 0; }
+  if (position == 1) { return 1; }
+  var first = 0,
+      second = 1,
+      next = first + second;
+  for (var i = 2; i < position; i++) {
+    first = second;
+    second = next;
+    next = first + second;
+  }
+  return next;
+}
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    return get_fib(position - 1) + get_fib(position - 2)
