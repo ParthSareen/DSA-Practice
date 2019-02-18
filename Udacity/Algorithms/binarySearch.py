fb76ab8fca87e8da0add6948a416c9566ab26118
ParthSareen <parth0612@live.com>
2019-02-17 21:09:15 -0500

    Adding more solutions, might even be becoming smarter, who knows!

--- Udacity/Algorithms/binarySearch.py
+++ Udacity/Algorithms/binarySearch.py
@@ -24,7 +24,7 @@ def binary_search(input_array, value):
     low = 0
     high = len(input_array) - 1
     while (low <= high):
-        middle = (low + high) / 2
+        middle = (low + high) // 2
         if (input_array[middle] == value):
             return middle
         elif (input_array[middle] < value):
@@ -36,5 +36,5 @@ def binary_search(input_array, value):
 test_list = [1,3,9,11,15,19,29]
 test_val1 = 25
 test_val2 = 15
-print binary_search(test_list, test_val1)
-print binary_search(test_list, test_val2)
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
